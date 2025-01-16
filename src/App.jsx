a13328c435135cf35e96aac5134dafd56364f66c
Harsh Vadher <153319594+Harshvadher05@users.noreply.github.com>
2025-01-16 23:08:38 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <div className="leftSide">
+          <img src="public\logo.png" alt="Your Company Logo" />
+          <div className="name">hey there!!</div>
+        </div>
+
+        <div className="rightSide">
+          <div className="header">
+            <h1>Log In</h1>
+          </div>
+          <div className="form">
+            <form action="">
+              <div className="info">
+                <input
+                  type="text"
+                  className="text"
+                  placeholder="Username"
+                  required
+                />
+              </div>
+              <div className="info">
+                <input
+                  type="Password"
+                  className="text"
+                  placeholder="Password"
+                  required
+                />
+              </div>
+              <div className="check">
+                <input type="checkbox" id="checkbox" required />
+                <label htmlFor="checkbox"> Remember Me!</label>
+              </div>
+              <button className="submit">Submit</button>
+            </form>
+          </div>
+          <div className="register">
+            <p>Don't have an account?</p>
+            <a href="#">Register Here</a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
